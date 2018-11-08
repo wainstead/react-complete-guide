714db87e6d7091624e8ed4c00263eae15a85b88d
Steve Wainstead <swain@panix.com>
2018-11-08 12:42:17 -0500

    Removed a single quote to satisfy Emacs syntax highlighting

--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,3 +1,4 @@
+
 import React from 'react';
 import './Person.css';
 
@@ -5,7 +6,7 @@ const person = (props) => {
     //return <p>I'm a Person and I am {Math.floor(Math.random() * 30)} years old!</p>
     return (
         <div className="Person">
-            <p onClick={props.click}>I'm a Person and my name is {props.name}. I am {props.age}.</p>
+            <p onClick={props.click}>I am a Person and my name is {props.name}. I am {props.age}.</p>
             <p>{props.children}</p>
             <input type="text" onChange={props.changed} value={props.name}/>
     </div>
