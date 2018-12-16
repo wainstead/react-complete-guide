e22d149a9cd477b1d9c16dafc20bf7d0fcb285d4
Steve Wainstead <swain@panix.com>
2018-12-16 11:41:22 -0500

    Updated the Person class to use CSS modules

--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,11 +1,11 @@
 
 import React from 'react';
-import './Person.css';
+import cssclasses from './Person.css';
 
 const person = (props) => {
     //return <p>I'm a Person and I am {Math.floor(Math.random() * 30)} years old!</p>
     return (
-        <div className="Person">
+        <div className={cssclasses.Person}>
             <p onClick={props.click}>I am a Person and my name is {props.name}. I am {props.age}.</p>
             <p>{props.children}</p>
             <input type="text" onChange={props.changed} value={props.name}/>
